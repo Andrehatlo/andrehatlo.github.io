d27600a274b90d5f942e112d5e112235d6d4fc70
André Hatlo <andre@rorkjop.no>
2019-01-07 17:52:45 +0100

    Update post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -49,18 +49,18 @@ var store = [{
         "tags": [],
         "url": "http://localhost:4000/lorem/ipsem/",
         "teaser":"http://localhost:4000/assets/images/me.png"},{
+        "title": "Lorem Ipsem",
+        "excerpt":"Aenean id lacinia neque nec bibendum odio risus a arcu imperdiet metus id velit augue id magna iaculis quis, pretium quam iaculis quis, sit amet nibh ullamcorper nec, pretium quam nonummy ac, erat libero tristique tellus, turpis at pulvinar vulputate, sed nisl molestie nec bibendum odio risus erat libero tristique...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/lorem/ipsem/",
+        "teaser":"http://localhost:4000/assets/images/me.png"},{
         "title": "Jekyll: Creating a blog is easy and FREE",
         "excerpt":"So, i’ve chosen to start blogging my experiences/insights/thoughts as a developer. Both to learn and to share knowledge. Trying to find the right platform to start a blog was my biggest challenge. Google didn’t make in much easier, i have a feeling their algorithm favorites something called WORDPRESS. So i...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/jekyll-creating-blog/",
         "teaser":"http://localhost:4000/assets/images/me.png"},{
         "title": "Jekyll + Github: Free website hosting tutorial",
-        "excerpt":"Step 1:   Under construction  ","categories": [],
+        "excerpt":"Requirements: Terminal Check whether you have Ruby 2.1.0 or higher installed: $ ruby -v ruby 2.x.x Install Bundler: $ gem install bundler Create a local repository for your Jekyll site. (Jump to step 1 if this is done) Install git, see Set up Git Open terminal Initialize a new Git...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/jekyll-and-github-makes-love",
-        "teaser":"http://localhost:4000/assets/images/me.png"},{
-        "title": null,
-        "excerpt":"Aenean id lacinia neque nec bibendum odio risus a arcu imperdiet metus id velit augue id magna iaculis quis, pretium quam iaculis quis, sit amet nibh ullamcorper nec, pretium quam nonummy ac, erat libero tristique tellus, turpis at pulvinar vulputate, sed nisl molestie nec bibendum odio risus erat libero tristique...","categories": [],
-        "tags": null,
-        "url": "http://localhost:4000/2019-01-01-lorem/",
         "teaser":"http://localhost:4000/assets/images/me.png"}]
