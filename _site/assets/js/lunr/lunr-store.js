0f8214a64eeab685615bb4fba838312c9e035789
André Hatlo <andre@rorkjop.no>
2019-01-09 09:36:57 +0100

    up

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -60,7 +60,7 @@ var store = [{
         "url": "http://localhost:4000/jekyll-and-github-makes-love",
         "teaser":"http://localhost:4000/assets/images/me.png"},{
         "title": "Hugo - as my static page blog",
-        "excerpt":"title: “Hugo - as my static page blog” date: 2019-01-09T08:45:32+01:00 draft: true — Hugo as a static page blog After the setup of my Jekyll site I came across Hugo. Hugo is a static site generator, on the same line as Jekyll as it uses Markdown to create blog posts....","categories": [],
+        "excerpt":"Hugo as a static page blog After the setup of my Jekyll site I came across Hugo. Hugo is a static site generator, on the same line as Jekyll as it uses Markdown to create blog posts. The main difference i noticed after testing Jekyll against Hugo is that it’s...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/hugo/",
         "teaser":"http://localhost:4000/assets/images/me.png"}]
